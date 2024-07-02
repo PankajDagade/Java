69ceb6d1edd2bb2bd6550e4f4879dce69941b804
PankajDagade <pankajdagade1998@gmail.com>
2024-07-02 15:39:22 +0530

    push by pankaj

--- Addmision/src/main/java/com/example/demo/AddmisionApplication.java
+++ Addmision/src/main/java/com/example/demo/AddmisionApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AddmisionApplication {
 
 	public static void main(String[] args) {
+		System.out.println("ABC");
 		SpringApplication.run(AddmisionApplication.class, args);
 	}
 
