1f375966b90cc902b49ad779e28fe0d3731fa16a
rishi942 <rishabhridhorkar@gmail.com>
2024-07-02 15:44:40 +0530

    push by rishb

--- Addmision/src/main/java/com/example/demo/AddmisionApplication.java
+++ Addmision/src/main/java/com/example/demo/AddmisionApplication.java
@@ -9,6 +9,7 @@ public class AddmisionApplication {
 	public static void main(String[] args) {
 		System.out.println("ABC");
 		System.out.println("PQR");
+		System.out.println("Pankaj");
 		SpringApplication.run(AddmisionApplication.class, args);
 	}
 
